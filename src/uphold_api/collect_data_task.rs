6b8b9620bab3230161e62a29b34043147d0a2052
Rodion Kovalenko <rodion_kovalenko@yahoo.de>
2021-08-30 08:20:25 +0200

    prediction tasks for crypto

--- src/uphold_api/collect_data_task.rs
+++ src/uphold_api/collect_data_task.rs
@@ -22,35 +22,35 @@ pub fn update_json_data_from_uphold_api() {
     loop {
         seconds_elapsed = now.elapsed().as_secs();
 
-        // if seconds_elapsed % COLLECT_PERIOD_IN_SECONDS == 0 {
-        //     thread::spawn(move || {
-        //         seconds_elapsed = now.elapsed().as_secs();
+        if seconds_elapsed % COLLECT_PERIOD_IN_SECONDS == 0 {
+            thread::spawn(move || {
+                seconds_elapsed = now.elapsed().as_secs();
 
-        //         println!("");
-        //         println!("this is thread number 1__________________start");
-        //         println!("time elapsed {:?}", seconds_elapsed);
+                println!("");
+                println!("this is thread number 1__________________start");
+                println!("time elapsed {:?}", seconds_elapsed);
 
-        //         cryptocurrency_api::update_currency_prices_from_uphold_web_api().expect("cannot update json file");
+                cryptocurrency_api::update_currency_prices_from_uphold_web_api().expect("cannot update json file");
 
-        //         println!("this is thread number 1__________________finish");
-        //         println!("");
-        //     });
-        // }
+                println!("this is thread number 1__________________finish");
+                println!("");
+            });
+        }
 
-        // if seconds_elapsed % BACKUP_PERIOD_IN_SECONDS == 0 {
-        //     thread::spawn(move || {
-        //         seconds_elapsed = now.elapsed().as_secs();
+        if seconds_elapsed % BACKUP_PERIOD_IN_SECONDS == 0 {
+            thread::spawn(move || {
+                seconds_elapsed = now.elapsed().as_secs();
 
-        //         println!("");
-        //         println!("this is thread number 2__________________start");
-        //         println!("time elapsed {:?}", seconds_elapsed);
+                println!("");
+                println!("this is thread number 2__________________start");
+                println!("time elapsed {:?}", seconds_elapsed);
 
-        //         file_utils::make_backup_file(cryptocurrency_api::FILE_NAME);
+                file_utils::make_backup_file(cryptocurrency_api::FILE_NAME);
 
-        //         println!("this is thread number 2__________________finish");
-        //         println!("");
-        //     });
-        // }
+                println!("this is thread number 2__________________finish");
+                println!("");
+            });
+        }
 
         if seconds_elapsed % CRYPTO_PREDICTIONS_IN_SECONDS == 0 {
             thread::spawn(move || {
