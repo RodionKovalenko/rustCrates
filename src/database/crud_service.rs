25f5509ba70c37acd95dd2b66f763ca0f9c608a4
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2024-12-15 21:26:28 +0100

    tokenizer, datababe sled

---
+++ src/database/crud_service.rs
@@ -0,0 +1,76 @@
+use sled::{self, IVec};
+use std::convert::TryInto;
+
+use super::sled_config::{get_storage_path, SLED_DB_TOKENIZER};
+
+pub fn insert_token(token: &str) -> Result<(), sled::Error> {
+    // Example data to store: Text as key and number as value
+    let key = token.as_bytes(); // The text key, converted to bytes
+    let mut v: i64 = 1;
+
+    let existing_value = get_value(token).unwrap();
+    // Open a Sled database (it will create a new one if it doesn't exist)
+    let db = sled::open(get_storage_path(SLED_DB_TOKENIZER))?;
+
+    if existing_value == 0 {
+        let iter = db.iter().rev(); // Reverse iteration
+
+        for entry in iter {
+            let (k, val) = entry?;
+            v = convert_bytes_to_i64(val).unwrap();
+            v += 1;
+            break; // Only process the first (most recent) entry
+        }
+
+        let value: [u8; 8] = v.to_be_bytes(); // The number value, converted to bytes (using Big-Endian)
+
+        // Convert the fixed-size array [u8; 4] into a slice and then to IVec
+        let value_vec = sled::IVec::from(&value[..]); // Convert to a slice first
+
+        // Insert the key-value pair into the database
+        db.insert(key, value_vec)?;
+    }
+
+    // Explicitly drop the database (although it would be dropped automatically)
+    drop(db);
+
+    Ok(())
+}
+
+pub fn get_value(token: &str) -> Result<i64, sled::Error> {
+    // Open a Sled database (it will create a new one if it doesn't exist)
+    let db = sled::open(get_storage_path(SLED_DB_TOKENIZER))?;
+
+    let key = token.as_bytes();
+    let mut number: i64 = 0;
+
+    // Retrieve the value back from the database using the key
+    if let Some(retrieved_value) = db.get(key)? {
+        // Ensure the length is correct (i.e., 4 bytes for an i32)
+        number = convert_bytes_to_i64(retrieved_value).unwrap();
+    }
+
+    // Explicitly drop the database (although it would be dropped automatically)
+    drop(db);
+
+    Ok(number)
+    // Close the database (it will be automatically flushed when it goes out of scope)
+}
+
+pub fn convert_bytes_to_i64(bytes: IVec) -> Result<i64, sled::Error> {
+    if bytes.len() == 8 {
+        // Try to convert the bytes into an i64 in big-endian format
+        let number = i64::from_be_bytes(bytes.as_ref().try_into().map_err(|_| {
+            sled::Error::Io(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Failed to convert slice",
+            ))
+        })?);
+        Ok(number)
+    } else {
+        Err(sled::Error::Io(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Expected 8 bytes for i64 conversion",
+        )))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::env;
 #[allow(unused_imports)]
 pub mod uphold_api;
 
-use neural_networks::neural_networks::{
-    network_components::layer::{create_default_layer, ActivationType, LayerEnum, LayerType},
-    network_types::{
-        feedforward_network_generic::{create, FeedforwardNetwork},
-        network_trait::Network, transformer::attention_layer::AttentionLayer,
+use neural_networks::{
+    database::crud_service::{get_value, insert_token},
+    neural_networks::{
+        network_components::layer::{create_default_layer, ActivationType, LayerEnum, LayerType},
+        network_types::{
+            feedforward_network_generic::{create, FeedforwardNetwork},
+            network_trait::Network,
+            transformer::attention_layer::AttentionLayer,
+            wavelet_network::decompose_in_wavelet_2d_default,
+        }, utils::tokenizer::{detokenize, tokenize},
     },
 };
 #[allow(unused_imports)]
@@ -63,14 +68,40 @@ fn main() {
     );
 
     let layer = create_default_layer::<4, 4>(&ActivationType::LEAKYRELU, LayerType::HiddenLayer);
-    let layer =  AttentionLayer::<4, 4>::create_default_attention_layer(ActivationType::LEAKYRELU, LayerType::HiddenLayer);
+    let layer = AttentionLayer::<4, 4>::create_default_attention_layer(
+        ActivationType::LEAKYRELU,
+        LayerType::HiddenLayer,
+    );
 
-    println!("{:?}", layer);
+    //println!("{:?}", layer);
+    let _ = insert_token("A");
+    let value = get_value("A").unwrap();
+    println!("value is {}", &value);
+
+    let _ = insert_token("B");
+    let value = get_value("B").unwrap();
+    println!("value is {}", &value);
 
 
+    let _ = insert_token("C");
+    let value = get_value("C").unwrap();
+    println!("value is {}", &value);
 
-    // let m1: Vec<f64> = vec![1.0, 2.0, 3.0];
-    // let m2: Vec<Vec<f64>> = vec![vec![5.0, 6.0, 7.0], vec![7.0, 8.0, 9.0]];
+    let _ = insert_token("D");
+    let value = get_value("D").unwrap();
+    println!("value is {}", &value);
+
+
+    let (tokens, ids) = tokenize("Hallo, wie geht es dir? Как твои дела? В мене справи добре").unwrap();
+
+    println!("tokens: {:?}", &tokens.len());
+    println!("ids: {:?}", &ids.len());
+
+    let decoded_text = detokenize(&ids).unwrap();
+    println!("decoded text: {:?}", decoded_text);
+
+    // let m1: Vec<f64> = vec![10000002222233334343434.0, 20000000000000000000000000.0, 3013333333333333333333333333333333333333333333.0];
+    // let m2: Vec<Vec<f64>> = vec![vec![56666666666666666666666666.0, 64444444444444444444444.0, 74463357654666.0], vec![776444545454545454545.0, 8545454545444.0, 954545454545454545.0]];
 
     // let result = decompose_in_wavelet_2d_default(&m1);
 
