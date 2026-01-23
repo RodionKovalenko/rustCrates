6b9c58392fe3c40e4d85d48140adbc8be59fc389
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2026-01-23 19:08:19 +0100

    shared embedding layer

--- src/neural_networks/network_types/transformer/transformer_network.rs
+++ src/neural_networks/network_types/transformer/transformer_network.rs
@@ -30,7 +30,7 @@ use crate::{
 pub const MAX_CONTEXT_WINDOW_SIZE: usize = 50280;
 pub const CONTEXT_OVERLAPPING: usize = 16;
 pub const EMA_SCALER: f64 = 1.1;
-pub const TOP_K_SIZE: usize = 100;
+pub const TOP_K_SIZE: usize = 300;
 
 #[inline]
 fn complex_batch_to_real_batch(data: &[Vec<Vec<C>>]) -> Vec<Vec<Vec<Real>>> {
