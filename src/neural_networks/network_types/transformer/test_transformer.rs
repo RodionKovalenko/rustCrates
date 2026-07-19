454b7a256415ec6a13ca510399ca0967f4d6a787
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2026-07-19 13:11:53 +0200

    refactoring

--- src/neural_networks/network_types/transformer/test_transformer.rs
+++ src/neural_networks/network_types/transformer/test_transformer.rs
@@ -105,7 +105,7 @@ pub fn test_train_transformer() {
     let dataset = Dataset::new(input, target);
     let num_epochs: usize = 5000;
 
-    train(&mut transformer, dataset, num_epochs, 1);
+    train(&mut transformer, dataset, num_epochs, _batch_size);
     let seconds_elapsed_end = now.elapsed();
 
     println!("time elapsed in seconds: {:?}", seconds_elapsed_end - seconds_elapsed);
